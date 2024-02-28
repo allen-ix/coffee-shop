4d773587f4fa48dbad68dba3caf923fe424229e6
John Allen Dugelio <dugeliojohnallen@gmail.com>
2024-02-28 22:38:52 +0800

    commit initial

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css"> <!-- Your custom stylesheet -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-body" data-bs-theme="dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="imgs/logo.png" alt="Logo"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="menu.php">Menu</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.php">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="apps.php">Apps</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Contact</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="btn btn-outline-light" href="login.php">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="btn btn-outline-light" href="signup.php">Sign Up</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container text-left">
+        <p class="tagline"><strong>Discover the art of awakening,</strong></p>
+        <p class="sub-tagline">one aromatic cup at a time.</p>
+        <div class="button-container">
+            <a class="btn btn-outline-dark" href="order.php">Order Now</a>
+            <a class="btn btn-outline-dark" href="aboutus.php">Learn About Us</a>
+        </div>
+    </div>
+
+
+    </header>
+    <body>
+        
+    <div class="video-background">
+            <video autoplay muted loop poster="poster.jpg">
+                <source src="imgs/bg5.mp4" type="video/mp4">
+                Your browser does not support HTML5 video.
+            </video>
+    </div>
+
+    <div id="carouselExampleFade" class="carousel slide carousel-fade">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="imgs/americano.jpg" class="d-block w-100" alt="americano">
+                </div>
+                <div class="carousel-item">
+                <img src="imgs/espresso.jpg" class="d-block w-100" alt="espresso">
+                </div>
+                <div class="carousel-item">
+                <img src="imgs/capuccino.jpg" class="d-block w-100" alt="capuccino">
+                </div>
+                <div class="carousel-item">
+                <img src="imgs/icedcoffee.jpg" class="d-block w-100" alt="icedcoffee">
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+    </div>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
+
